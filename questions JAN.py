1cd73032be342911a331f630c1f668d9953ca59c
vicas <25vicentecatarino.acc@lwetb.ie>
2026-01-28 14:59:00 +0000

    Add files via upload

---
+++ questions JAN.py
@@ -0,0 +1,26 @@
+number = input ("please input a number")
+number = float (number)
+number2 = input ("please input a second number")
+number2 = float (number2)
+average = (number + number2)/ 2
+average = float (average)
+print ("your average of number is:",average)
+
+
+length = input ('enter the length of a box')
+length = float (length)
+width = input ('enter the width of a box')
+width = float (width)
+height = input ('enter the height of a box')
+height = float (height)
+total = (length * height * width)
+total = float (total)
+print ('great! your box has the volume of: ',total)
+
+length1 = float(input ('please enter the length of your triangle'))
+base = float(input('please enter the base of the triangle'))
+height1 = float(input('please enter the height of your triangle'))
+area = (0.5 * base * height)
+area = float (area)
+print ('the area of your triangle is :',area)
+
