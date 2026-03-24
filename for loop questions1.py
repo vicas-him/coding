6a04d535a2daa43bc2de80e78ef75e531c3f8a4d
vicas <25vicentecatarino.acc@lwetb.ie>
2026-03-24 14:51:06 +0000

    Add files via upload

---
+++ for loop questions1.py
@@ -0,0 +1,39 @@
+for count in range(1,31):
+     print(count*"$")
+ 
+ for x in range(100):
+     print("welcome")
+ print ("bye")
+ 
+ 
+ for number in range(1,101):
+     print (number)
+   
+     
+ number1 = input ('enter a number for it multiples up to 100')
+ for V in range (1,101):
+     number1 = int (number1)
+     print (number1*V)
+ 
+ 
+ number2 = input ('enter a number') 
+ number2 = int (number2)
+ for E in range (number2):
+     print ('i like turtles')
+
+ name = input ('enter a name you want printed')
+ number3 = input ('enter the number of times you want your name printed') 
+ number3 = int (number3)
+ for Z in range (number3):
+     print (name)
+     
+     
+number4 = input ('enter a number for multiplication table')
+number4 = int (number4)
+for c in range (number4):
+    
+    
+
+
+
+
