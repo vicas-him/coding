0de014f2e12bc2028f969de935afd8f6849e05f2
vicas <25vicentecatarino.acc@lwetb.ie>
2026-03-25 15:32:40 +0000

    Add files via upload

--- revision 1.py
+++ revision 1.py
@@ -1,11 +1,17 @@
-a = int(input('enter an integer value :'))
-b = int(input('enter another integer value :'))
-c = int(input('enter one last integer value :'))
+for a in range (1,21):
+    for b in range (1,21):
+        for c in range (1,21):
+            a_squared = a ** 2
+            b_squared = b ** 2
+            c_squared = c ** 2 
 
-a_squared = a ** 2
-a_squared = int (a_squared)
-b_squared = b ** 2
-b_squared = int (b_squared)
-c_squared = (a_squared + b_squared)
-c_squared = int (c_squared)
-print ('your answer is ',c_squared)
+    print('your numbers are ',a_squared, b_squared , c_squared)
+    
+
+
+    
+
+    
+
+
+            
