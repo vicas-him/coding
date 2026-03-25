dd5e6945fc2da6367be6c304e4551bee43f20219
vicas <25vicentecatarino.acc@lwetb.ie>
2026-03-25 15:35:15 +0000

    Add files via upload

---
+++ revision 2.py
@@ -0,0 +1,9 @@
+for aa in range (1,21):
+    for bb in range (1,21):
+        for cc in range (1,21):
+            aa_squared = a ** 2
+            bb_squared = b ** 2
+            cc_squared = c ** 2 
+
+    print('your numbers are ',aa_squared, bb_squared , cc_squared)
+
