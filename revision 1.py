a5173d2f58e3a03cb591953560e61a454f2987a5
vicas <25vicentecatarino.acc@lwetb.ie>
2026-03-25 14:59:33 +0000

    Add files via upload

---
+++ revision 1.py
@@ -0,0 +1,11 @@
+a = int(input('enter an integer value :'))
+b = int(input('enter another integer value :'))
+c = int(input('enter one last integer value :'))
+
+a_squared = a ** 2
+a_squared = int (a_squared)
+b_squared = b ** 2
+b_squared = int (b_squared)
+c_squared = (a_squared + b_squared)
+c_squared = int (c_squared)
+print ('your answer is ',c_squared)
