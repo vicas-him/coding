94adf1166f2cec1df146aa6d643fb86863f40298
vicas <25vicentecatarino.acc@lwetb.ie>
2026-05-05 14:57:46 +0100

    Add files via upload

---
+++ home wrk/coding hwrk.py
@@ -0,0 +1,53 @@
+1#
+num1 = input ('enter a number')
+num1 = float (num1)
+num2 = input ('enter a second number')
+num2 = float (num2)
+
+sum = num1 + num2
+diff = num1 - num2
+multi = num1 * num2
+divide = num1/ num2
+
+print (sum)
+print (diff)
+print (multi)
+print (divide)
+
+2#
+len = input ('enter the length of a square')
+len = float (len)
+width = input ('enter the length of a square')
+width = float (width)
+area = len * width
+
+print ('your squares length is ',len)
+print ('your squares width is ',width)
+print ('your squares area is ',area)
+3#
+len1 = input ('enter the length of a rectangle')
+len1 = float (len1)
+width1 = input ('enter the length of a rectangle')
+width1 = float (width1)
+pri = len1 + len1 + width1 +width1
+
+print ('your rectangle length is ',len1)
+print ('your rectangle width is ',width1)
+print ('your rectangle perimeter is ',pri)
+4#
+radius = input ('input the radius of your circle')
+radius = float (radius)
+PI = 3.14
+PIRAD = PI * radius * radius
+print ('the area of your circle is:',PIRAD)
+
+
+radius1 = input ('input the radius of your cylinder')
+radius1 = float (radius1)
+height = input ('input the height of your cylinder')
+height = float (height)
+PI1 = 3.14
+PIRH = PI1 * radius * radius * height
+print ('the volume of your cylinder is:',PIRH)
+
+
