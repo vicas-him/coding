41bf103e553b61fc6246c994e752081126edca45
vicas <25vicentecatarino.acc@lwetb.ie>
2026-05-06 15:38:01 +0100

    Add files via upload

---
+++ for loop questions2.py
@@ -0,0 +1,16 @@
+x=0
+a=-0
+for dot in range (1,11):
+    print (dot)
+    x = x + dot
+    a = a + dot / 10
+print (x)
+print (a)
+
+inp = int(input('type in a number'))
+for c in range (1,13):
+    print (c,'the multiplication table is',c * inp)
+
+name = input ('enter a word')
+for x in name:
+    print (x)
